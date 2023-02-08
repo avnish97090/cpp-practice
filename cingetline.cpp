633038b6022039c2725b1f43f43ee98be06ce4d1
ABANISH <avnish97090@gmail.com>
2023-02-09 01:59:57 +0530

    reverseatringg'

---
+++ cingetline.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[100];
+    cin.getline(name,20 ,'r');
+    cout<<name<<endl;
+
+}
---
+++ reversestring.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int stringLength(char input[]){
+    int count = 0;
+    for( int i = 0; input[i]!='\0'; i++){
+          count++;
+    }
+    return count;
+}
+void stringReverse(char input[]){
+    int i = 0; int len = stringLength(input);
+    int j = len -1 ;
+    while(i<j){
+           char temp;
+           temp = input[i];
+           input[i]= input[j];
+           input[j]= temp;
+           i++;
+           j--;
+    }
+     
+}
+int main(){
+    char name[100];
+    cin.getline(name,30);
+    cout<<name<<endl;
+    stringLength(name);
+    stringReverse(name);
+    cout<<name<<endl;
+}
